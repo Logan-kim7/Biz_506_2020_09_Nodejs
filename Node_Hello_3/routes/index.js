9b05f8e1bc8f45500be97a8630ed77ba35d7b70a
skynate22 <skynate22@naver.com>
2020-09-16 16:53:49 +0900

    Nodejs homework

---
+++ Node_Hello_3/routes/index.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var router = express.Router();
+const moment = require("moment");
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  let date = moment(new Date()).format("YYYY-MM-DD");
+  let time = moment(new Date()).format("hh:mm:ss");
+  let title = "반갑습니다";
+  res.render("index", {
+    title: title,
+    date: date,
+    time: time,
+  });
+});
+
+router.post("/", function (req, res) {
+  let name = req.body.name;
+
+  let date = moment(new Date()).format("YYYY-MM-DD");
+  let time = moment(new Date()).format("hh:mm:ss");
+  let title = name + "님 반갑습니다";
+
+  res.render("index", {
+    title: title,
+    date: date,
+    time: time,
+  });
+});
+
+module.exports = router;
--- Node_bbs/routes/bbsRoute.js
+++ Node_bbs/routes/bbsRoute.js
@@ -1,3 +1,4 @@
+const { request } = require("express");
 var express = require("express");
 var router = express.Router();
 /*
@@ -32,8 +33,11 @@ router.get("/list", function (req, res) {
 
 //localhost:3000/bbs/write URL 요청
 router.get("/write", function (req, res) {
-  // bbsWrite.pug파일을 전송
-  res.render("bbsWrite");
+  // bbsWrite.pug파일을 rendering 하여 요청전송
+  // insert와 update를 write.pug 파일로 공통으로 사용하기 위해서
+  // insert를 수행할때 비어있는 vo를 만들어서 bbsWrtie에 전달해줘야 한다.
+  let data = new bbsVO();
+  res.render("bbsWrite", { bbsVO: data });
 });
 // form에 데이터를 입력하고 전송버튼을 클릭했을때 호출되는 URL
 // form, input에 입력된 데이터를 담아서 보내면 그 데이터를
@@ -52,10 +56,17 @@ router.post("/write", function (req, res) {
   // req.body.b_write = "홍길동";
   req.body.b_count = 0;
 
+  // form에 전송받은 데이터를 통째로 bbs(data) 객체로 생성하라
+  // data 에는 form에서 입력하여 전송한 데이터 (작성자, 제목, 내용)과
+  // 서버에서 임의로 추가한 데이터(날짜, 시간, 카운트)를 모두 모아서
+  // 하나의 VO 객체로 생성을 하고
+  // 거기에 DB CRUD와 관련된 함수들을 함께 추가하여 객체 생성함
+
   let data = new bbsVO(req.body);
   //data Create 하기
-  data //생성한 bbsVO(data)에 저장된 데이터를 mongoDB의 table에 insert
-    .save()
+
+  data
+    .save() //생성한 bbsVO(data)에 저장된 데이터를 mongoDB의 table에 insert
     .then(function (bbsVO) {
       //insert가 성공하면
       res.redirect("/bbs/List"); //client 에게 다시 데이터를 보여라
@@ -75,7 +86,7 @@ router.post("/write", function (req, res) {
 // id값 : bbs의 각 라인(item)의 PK값
 // PK값을 가지고 tbl_bbs에서 1개의 item 값을 추출하여
 // detail view에 보여주기
-router.get("/view/:_id", function (req, res) {
+router.get("/view/:id", function (req, res) {
   let id = req.params.id;
 
   // findOne : findById()
@@ -91,6 +102,85 @@ router.get("/view/:_id", function (req, res) {
     })
     .catch(function (error) {
       console.error(error);
+    });
+});
+
+/*
+데이터를 삭제할때
+mongoDB 3.x 이하에서는 remove(조건문) 함수를 사용했다.
+조건문 없이 remove() 를 실행하면 테이블의 모든 데이터를 묻지도 따지지도 않고 삭제해버린다.
+mongoDB 4.x 이상에서는 이러한 문제를 방지하기 위해서 
+deleteOne deleteMany()라는 함수를 만들어 두었다.
+deleteOne(조건문)은 조건에 일치하는 데이터중 첫번째 한개만 삭제를 수행
+deleteMany(조건문)은 조건에 일치하는 데이터를 모두 삭제
+
+
+
+*/
+router.get("/delete/:id", function (req, res) {
+  let id = req.params.id;
+  bbsVO
+    /*
+    mongoDB의 item(1개의 데이터를) 삭제하기
+    deleteOne(조건문): 삭제한 후 삭제되었다는 메시지만 알고 싶을때
+    findOneAndDelete(조건문): 삭제를 수행한 후 실제 삭제된 데이터를
+        다시 확인하고 싶을때 
+   */
+    // .deleteOne({_id: id})
+    .findOneAndDelete({ _id: id })
+    .then(function (result) {
+      // 일반적인 CRUD 프로젝트에서는
+      // 삭제 동작이 완료되면 List 보기로 redirect를 수행한다
+      // res.json(result);
+      res.redirect("/bbs/list");
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+});
+
+//전달받은 PK 값으로 한개의 item을 조회하고
+// write form에 보여주고
+// 데이터를 입력한 다음 저장을 하면
+// post("/update")로 보내서 데이터를 수정하도록 수행
+router.get("/update/:id", function (req, res) {
+  let id = req.params.id;
+  bbsVO.findOne({ _id: id }).then(function (result) {
+    res.render("bbsWrite", { bbsVO: result });
+  });
+});
+
+/*
+한개의 item을 조회하여 write form으로 보낸 후 데이터를 다시 받았을때
+req.body에는 id이 사라진 상태로 전달된다.
+하지만 update를 수생할때 id값을 URL에 전닥하여 보냈기 때문에 
+for(method="POST",actipn="/bbs/update/id값") 형태의 URL이 자동으로 만들어져
+post Method는 params에 id 값을 받아서 수신하는 결과가 된다.
+
+
+params로 부터 id를 추출하여 강제로 req.body에 추가를 해준다.
+*/
+router.post("/update/:id", function (req, res) {
+  let id = req.params.id;
+  req.body._id = id;
+
+  bbsVO
+    .updateOne(
+      { _id: id }, //where 조건문
+      {
+        // input box에 입력한 데이터만 변경하도록 수행하는 코드
+        // SET 에 해당하는 방법
+        // 주의 해야하는 코드
+        // $set {req.body}
+        // update를 수행해야하는데 실제로 form에서 전달된 데이터외에
+        // 나머지 데이터는 모두 삭제해버리는 문제가 있다.
+        b_title: req.body.b_title,
+        b_write: req.body.b_write,
+        b_text: req.body.b_text,
+      }
+    )
+    .then(function (result) {
+      res.redirect("/bbs/list");
     });
 });
 
